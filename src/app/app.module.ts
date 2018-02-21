c17b1bd296049dc9dc3afc78e8990f64a583ab00
kawa97 <kawa.luca97@gmail.com>
2018-02-21 11:09:44 +0100

    routing pacchissimo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListaComponent } from './lista/lista.component';
+import { ModificaComponent } from './modifica/modifica.component';
 
+const appRoutes: Routes = [
+  
+  
+  { path: 'home', component: HomeComponent },
+  
+  { path: 'lista', component: ListaComponent },
+
+  { path: 'modifica', component: ModificaComponent },
+  
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListaComponent,
+    ModificaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
