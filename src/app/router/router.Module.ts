393409a60d76aa6ee362042d33e82ae9407184b3
kawa97 <landauro.kelly@gmail.com>
2018-02-23 13:02:00 +0100

    authguard non funzionante da sistemare

--- src/app/router/router.Module.ts
+++ src/app/router/router.Module.ts
@@ -7,15 +7,27 @@ import { ModificaComponent } from '../modifica/modifica.component';
 import { DetailComponent } from '../detail/detail.component';
 import { NotfoundComponent } from '../notfound/notfound.component';
 import { LoginComponent } from '../login/login.component';
+import { AuthGuard } from '../AuthGuard';
     
     const routes: Routes = [
-        { path: 'login' , component:LoginComponent},
-        { path: 'home', component: HomeComponent },
         { path: '', redirectTo:'login' ,pathMatch:'full'},
-        { path: 'lista', component: ListaComponent },
-        { path: 'detail/:id', component: DetailComponent },
-        { path: '**', component: NotfoundComponent}
-        ];
+        {   path: 'login' , component:LoginComponent,
+        canActivateChild:[AuthGuard],
+            children:[
+            { path: 'home', component: HomeComponent,canActivate:[AuthGuard]},
+            { path: 'lista', component: ListaComponent },
+            { path: 'detail/:id', component: DetailComponent },
+            { path: '**', component: NotfoundComponent}
+                    ]
+        },
+                            ];
+
+
+
+
+
+        
+
     @NgModule({
     imports: [
     RouterModule.forRoot(
