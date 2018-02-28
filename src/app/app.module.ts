faaa3133ea2684b34a7dd480c1ddd9a2cd16fabb
GabrieleMoia <gabriele.moia@icloud.com>
2018-02-28 11:49:06 +0100

    direttiva base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HighLightDirective } from './high-light.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighLightDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/high-light.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHighLight]'
+})
+export class HighLightDirective {
+
+   constructor(el: ElementRef) {
+
+
+    el.nativeElement.style.background= "blue";
+
+  }
+}
